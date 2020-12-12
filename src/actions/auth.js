02cd2e9c5521431d63da083e6e0ab300b53726b5
nasir-bukhari <bukhari.nasir998@gmail.com>
2020-12-13 01:43:13 +0500

    Add Prototype Pages Without Funtionality

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -164,16 +164,26 @@ export const login = (email, password) => async (dispatch) => {
   }
 };
 
-export const signup = ({ first_name, last_name , email, password, re_password }) => async (
-  dispatch
-) => {
+export const signup = ({
+  first_name,
+  last_name,
+  email,
+  password,
+  re_password,
+}) => async (dispatch) => {
   const config = {
     headers: {
       "Content-Type": "application/json",
     },
   };
 
-  const body = JSON.stringify({ first_name, last_name, email, password, re_password });
+  const body = JSON.stringify({
+    first_name,
+    last_name,
+    email,
+    password,
+    re_password,
+  });
 
   try {
     const res = await axios.post(
